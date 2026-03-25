ba28ac46b6fc742bb9ff4c139e7a6aa52a829f32
Farhan khan <fiercy69@gmail.com>
2026-03-25 11:15:29 +0530

    Simplify direct_url assignment logic
    Refactor direct_url assignment based on query.mode.

--- player.py
+++ player.py
@@ -4,15 +4,15 @@ query.chosen = int(input("What to play:")) - 1
 query.to_play = query.results[query.chosen]["url"]
 print("Now playing:", query.results[query.chosen]["title"])
 if query.mode == 1:
+    direct_url = subprocess.check_output(["yt-dlp", "-f", aformat, "-g", query.to_play], text = True).strip()
     if query.aqual == 1:
         aformat = "bestaudio"
     elif query.aqual == 2:
         aformat = "bestaudio[abr<=128]"
     else:
         pass
-
-
 elif query.mode == 2:
+    direct_url = query.to_play
     if query.bitrate == 1:
         mpv_flag = "--ytdl-format=bestvideo[height<=1080]+bestaudio"
     elif query.bitrate == 2:
@@ -21,8 +21,5 @@ elif query.mode == 2:
         mpv_flag = "--ytdl-format=bestvideo[height<=360]+bestaudio"
     else:
         mpv_flag= "--ytdl-format=bestvideo+bestaudio"
-if query.mode == 1:
-    direct_url = subprocess.check_output(["yt-dlp", "-f", aformat, "-g", query.to_play], text = True).strip()
-if query.mode == 2:
-    direct_url = query.to_play
+        
 subprocess.run(["mpv", mpv_flag, direct_url])
